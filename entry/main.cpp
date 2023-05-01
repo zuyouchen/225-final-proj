125499f73a13c359016a3ac534e1766582ad97d6
Evan Chen <echen48@illinois.edu>
2023-05-01 01:15:18 +0000

    print out Dijkstra's CSV through main

--- entry/main.cpp
+++ entry/main.cpp
@@ -8,11 +8,20 @@ int main() {
     Graph g("data/nodes.csv", "data/prereqs.csv");
     // g.print();
     //g.VectorToCSV();
-    // Printing out Dijkstra's on FirstStep
+
+    // Dijkstra's on FirstStep
     vector<Graph::Node*> nodes = g.getNodes();
     vector<Graph::Node*> test_path = g.Dijkstra(nodes[0]);
-    for (auto& node : test_path) {
-        cout << node->name << '(' << node->time << ')' <<  " -> " << endl;;
+    // printing it out 
+    // for (auto& node : test_path) {
+    //     cout << node->name << '(' << node->time << ')' <<  " -> " << endl;;
+    // }
+
+    // write to CSV for bazookaCow
+    ofstream csv_file("data/dijkstras-output.csv");
+    for (auto& node : test_path ) {
+        csv_file << node -> name << "," <<  "\n";
     }
+    csv_file.close();
     return 0;
 }
