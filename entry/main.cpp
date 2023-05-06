16a49c956e627ea92ec65f4d22673d7b3cb825cc
mkaplan6 <102476703+mkaplan6@users.noreply.github.com>
2023-05-05 20:30:03 -0500

    Update main.cpp

--- entry/main.cpp
+++ entry/main.cpp
@@ -18,17 +18,23 @@ int main()
     cout << "Computed time to complete Anypercent route: " << g.getAnypercentTotal() << " seconds; " << g.getAnypercentTotal() / 60.00 << " minutes; " << g.getAnypercentTotal() / 3600.00 << " hours." << endl;
     cout << "" << endl;
 
+    Graph g2("data/nodes.csv", "data/prereqs.csv");
+    vector<Graph::Node *> nodes2 = g2.getNodes();
+
     cout << "All Remembrances via Dijkstra's running; outputted to output/allrem-output.csv for input to visualization" << endl;
-    vector<Graph::Node *> all_remembrances_route = g.Dijkstra(nodes[0], allremembrances);
-    g.VectorToCSV(all_remembrances_route, "output/allrem-output.csv", allremtime);
-    cout << "Computed time to complete All Remembrance Bosses route: " << g.getAllRemsTotal() << " seconds; " << g.getAllRemsTotal() / 60.00 << " minutes; " << g.getAllRemsTotal() / 3600.00 << " hours." << endl;
+    vector<Graph::Node *> all_remembrances_route = g2.Dijkstra(nodes2[0], allremembrances);
+    g2.VectorToCSV(all_remembrances_route, "output/allrem-output.csv", allremtime);
+    cout << "Computed time to complete All Remembrance Bosses route: " << g2.getAllRemsTotal() << " seconds; " << g2.getAllRemsTotal() / 60.00 << " minutes; " << g2.getAllRemsTotal() / 3600.00 << " hours." << endl;
     cout << "" << endl;
+
+    Graph g3("data/nodes.csv", "data/prereqs.csv");
+    vector<Graph::Node *> nodes3 = g3.getNodes();
 
     /* RUNNING BFS */
     cout << "BFS running; outputted to output/bfs-output.csv for input to visualization" << endl;
-    vector<Graph::Node *> BFSroute = g.BFS(nodes[0]);
-    g.VectorToCSV(BFSroute, "output/bfs-output.csv", bfs);
-    cout << "Computed time to complete BFS route: " << g.getBFSTotal() << " seconds; " << g.getBFSTotal() / 60.00 << " minutes; " << g.getBFSTotal() / 3600.00 << " hours." << endl;
+    vector<Graph::Node *> BFSroute = g3.BFS(nodes3[0]);
+    g3.VectorToCSV(BFSroute, "output/bfs-output.csv", bfs);
+    cout << "Computed time to complete BFS route: " << g3.getBFSTotal() << " seconds; " << g3.getBFSTotal() / 60.00 << " minutes; " << g3.getBFSTotal() / 3600.00 << " hours." << endl;
     cout << "" << endl;
 
     /* RUNNING Floyd-Warshall Samples */
